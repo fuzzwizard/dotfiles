d2c95afd71927d49aad16d7727cb857569e2000c
fuzzwizard <momojak@gmail.com>
2017-03-08 11:45:42 -0800

    add backup script

---
+++ scripts/brew-dump.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+which -s brew
+if [[ $? == 0 ]]
+then
+    brew bundle dump --force --file='~/.dotfiles'
+else
+    echo "Brew doesn't appear to be installed. You should do that!"
+    echo ''
+    echo '(In case you need help):'
+    echo '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+fi
