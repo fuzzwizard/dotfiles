057d08d214b7f3f63ce28cb2600bb32ce8498845
fuzzwizard <momojak@gmail.com>
2017-03-20 14:43:19 -0700

    add some scripts for migration

---
+++ scripts/BOOTSTRAPPER.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo """
+This script will initiate the migration session, including:
+
+Running brew on the Brewfile
+Symlinking various dotfiles (via symlinks.sh)
+Other things as I think of them
+"""
---
+++ scripts/symlinks.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo """
+Ideally this will sym up all the things that need to be symed for
+regular working (.zshrc, etc) but right now that ain't workin.
+"""
