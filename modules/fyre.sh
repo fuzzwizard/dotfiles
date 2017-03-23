7190c1b0a98e23e3b879c860cd33899cd65a22b7
fuzzwizard <momojak@gmail.com>
2017-03-23 12:47:59 -0700

    fix fyre functions

--- modules/fyre.sh
+++ modules/fyre.sh
@@ -11,21 +11,21 @@ __enforce_branch () {
 
 fyre_update () {
     local commands=(
-        "echo Checking out dev... && git checkout dev && "
-        "echo Pulling it down... && git pull && "
-        "echo Checking out $current_branch... && git checkout "$current_branch" && "
-        "echo Merging in dev... && git merge dev;"
+        'echo Checking out dev... && git checkout dev && '
+        'echo Pulling it down... && git pull && '
+        'echo Checking out $current_branch... && git checkout "$current_branch" && '
+        'echo Merging in dev... && git merge dev;'
     )
     __enforce_branch 'Merging `dev` into current branch' $commands
 }; alias fupdate="fyre_update"
 
 fyre_rebase () {
     local commands=(
-        "echo Checking out dev... && git checkout dev && "
-        "echo Pulling down dev... && git pull && "
-        "echo Checking out $current_branch... && git checkout $current_branch && "
-        "echo Rebasing... && git rebase -i dev && "
-        "echo Pushing up... && git push -f origin $current_branch;"
+        'echo Checking out dev... && git checkout dev && '
+        'echo Pulling down dev... && git pull && '
+        'echo Checking out $current_branch... && git checkout $current_branch && '
+        'echo Rebasing... && git rebase -i dev && '
+        'echo Pushing up... && git push -f origin $current_branch;'
     )
     __enforce_branch "Rebasing $current_branch into \`dev\`..." $commands
 }; alias frebase="fyre_rebase"
