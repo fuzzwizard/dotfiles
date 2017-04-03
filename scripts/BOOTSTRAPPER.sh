f517be121c3060f58177c6fcc5e5dc31302b4210
fuzzwizard <mitch@fyreapp.com>
2017-04-03 08:57:45 -0700

    remove bootstrapper

--- scripts/BOOTSTRAPPER.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo """
-This script will initiate the migration session, including:
-
-Running brew on the Brewfile
-Symlinking various dotfiles (via symlinks.sh)
-Other things as I think of them
-"""
