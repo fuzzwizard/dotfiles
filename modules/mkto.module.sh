159253545696a5bbe677cd4a8633e692679dbbae
Mitch Small <msmall@marketo.com>
2017-08-07 10:38:55 -0700

    clean up some files

--- modules/mkto.module.sh
+++ modules/mkto.module.sh
@@ -8,15 +8,16 @@ alias mlm_stop="cd $WORK_DIR/docker-compose/mercury; \
   identity redis mongo memcached assetapi treeservice metadataservice\
   mercury-server"
 
-alias mcl_start="cd $WORK_DIR/docker-compose/mercury &&\
+alias hgclient_start="cd $WORK_DIR/docker-compose/mercury &&\
   nohup docker-compose up mercury-client >/dev/null &"
 
-alias mcl_stop="cd $WORK_DIR/docker-compose/mercury;\
+alias hgclient_stop="cd $WORK_DIR/docker-compose/mercury;\
   docker-compose stop mercury-client"
 
 alias mlm_tail="tail -f $WORK_DIR/docker-compose/mercury/nohup.out"
-alias mcl_tail="docker logs -f mercury_mercury-client_1 --tail 100"
+alias hgclient_tail="docker logs -f mercury_mercury-client_1 --tail 100"
+alias hgserver_tail="docker logs -f mercury_mercury-server_1 --tail 100"
 
 # This sleep 60 is so bad that I am embarassed to commit it.
 alias mlm="mlm_stop; sleep 60; mlm_start; mlm_tail;"
-alias storybook="work; cd mercury-storybook; yarn && yarn storybook;"
+alias hg_storybook="work; cd mercury-storybook; yarn && yarn storybook;"
