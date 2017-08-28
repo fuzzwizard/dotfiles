30864fec15ac294a48d2feef13e5db75de9f6129
Mitch Small <msmall@marketo.com>
2017-08-28 11:19:16 -0700

    update mkto

--- modules/mkto.module.sh
+++ modules/mkto.module.sh
@@ -1,12 +1,11 @@
+alias dc_pull="pushd $WORK_DIR/docker-compose/mercury;\
+  docker-compose pull; popd"
+
 alias dc_start="pushd $WORK_DIR/docker-compose/mercury;\
-  nohup docker-compose up nginx-proxy mlm mlm-mysql-data mlm-mysql zookeeper\
-  locator identity redis mongo memcached assetapi treeservice metadataservice\
-  mercury-server &; popd"
+  docker-compose up &; popd"
 
 alias dc_stop="pushd $WORK_DIR/docker-compose/mercury; \
-  docker-compose stop nginx-proxy mlm mlm-mysql-data mlm-mysql zookeeper locator\
-  identity redis mongo memcached assetapi treeservice metadataservice\
-  mercury-server; popd"
+  docker-compose stop; popd"
 
 alias hgclient_start="pushd $WORK_DIR/docker-compose/mercury &&\
   nohup docker-compose up mercury-client >/dev/null &; popd"
@@ -19,6 +18,5 @@ alias hgclient_tail="docker logs -f mercury_mercury-client_1 --tail 100"
 alias hgserver_tail="docker logs -f mercury_mercury-server_1 --tail 100"
 
 # This sleep 60 is so bad that I am embarassed to commit it.
-alias mlm="mlm_stop; sleep 60; mlm_start; mlm_tail;"
 alias hg_storybook="work; cd mercury-storybook; yarn && yarn storybook;"
 alias secret_crimes="rm -rf ~/Secret\ Crimes/*"
