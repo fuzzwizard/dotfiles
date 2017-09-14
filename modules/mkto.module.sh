c9d12f6083e41cb5fbf4324021ab7f9f2b6782ca
Mitch Small <msmall@marketo.com>
2017-09-14 09:58:50 -0700

    yay! its good now!

--- modules/mkto.module.sh
+++ modules/mkto.module.sh
@@ -1,44 +1,17 @@
-DC="docker-compose"
 unalias d
-alias d="$DC"
+alias d="docker-compose"
 
-__in () {
-	pushd "/Users/msmall/Work/docker-compose/mercury" > /dev/null;
+YAML_DIR="/Users/msmall/Work/docker-compose/mercury"
+
+dd () {
+	if [[ "$(pwd)" != $YAML_DIR ]]; then
+		pushd $YAML_DIR > /dev/null;
+	else
+		popd > /dev/null;
+	fi
 }
 
-__out () {
-	popd > /dev/null;
-}
-
-__dc_do_one () {
-	__in;
-	docker-compose "$@";
-	__out;
-}
-
-alias dc_pull="__dc_do_one pull"
-
-alias dc_up="__in; $DC up &; __out;"
-alias dc_up_block="__dc_do_one up"
-
-alias dc_stop="__dc_do_one stop"
-
-alias dc_flush="__in; \
-  $DC stop; $DC rm; $DC pull; __out"
-
-alias dc_restart=" __in; \
-  $DC stop; $DC up; __out"
-
-alias dc_pullrestart="__in; \
-  $DC stop; $DC pull; $DC up; __out"
-
-alias hgclient_start="__in &&\
-  nohup $DC up mercury-client >/dev/null &; __out"
-
-alias hgclient_stop="__in; \
-  $DC stop mercury-client; __out"
-
-alias mlm_tail="tail -f $WORK_DIR/$DC/mercury/nohup.out"
+alias mlm_tail="tail -f $YAML_DIR/nohup.out"
 alias hgclient_tail="docker logs -f mercury_mercury-client_1 --tail 100"
 alias hgserver_tail="docker logs -f mercury_mercury-server_1 --tail 100"
 
