0c7206ea91cfc1813710cb0261044faaf4a0474c
Mitch Small <msmall@marketo.com>
2018-04-30 12:56:38 -0700

    made changes

--- modules/brew.module.sh
+++ modules/brew.module.sh
@@ -1,9 +1,8 @@
 alias brew_backup="brew leaves > ~/.brew-backup;\
 brew cask list > ~/.brew-cask-backup"
 
-alias brew_update="brew update; brew upgrade; brew doctor;"
-
-alias brew_clean="brew cleanup; brew cask cleanup;"
+alias __brew_clean="brew cleanup; brew cask cleanup;"
+alias brew_update="brew update && brew upgrade; __brew_clean; brew doctor;"
 
 brew_dump() {
   brew bundle dump --force --file=$1;
