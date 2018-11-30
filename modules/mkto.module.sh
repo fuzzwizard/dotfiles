4423a8fc4f4fe25b16ccd2608cb12dd476e9844e
Mitch Small <msmall@marketo.com>
2018-11-30 15:59:42 -0800

    [skip ci] get it workin

--- modules/mkto.module.sh
+++ modules/mkto.module.sh
@@ -1,52 +1,45 @@
 alias d="docker-compose"
 
 YAML_DIR="/Users/msmall/Work/docker-compose/mercury"
-YAML_DIR="/Users/msmall/Work/docker-compose/mercury-min"
+YAML_MIN_DIR="/Users/msmall/Work/docker-compose/mercury-min"
 export DOCKER_COMPOSE_LOCATION="$YAML_DIR"
 export COMPOSE_HTTP_TIMEOUT='1000'
 
 __DD_POCKET=""
 
-dd () {
-	if [[ "$(pwd)" != $YAML_DIR ]]; then
+___dd () {
+	if [[ "$(pwd)" != $1 ]]; then
 		__DD_POCKET="$(pwd)";
-		cd $YAML_DIR;
+		cd $1;
 	elif [[ -n $__DD_POCKET ]]; then
 		cd $__DD_POCKET;
 		__DD_POCKET="";
 	else;
 		echo "Nowhere to navigate.";
 	fi;
+}
+
+dd () {
+	___dd $YAML_DIR;
 }
 
 ddm () {
-	if [[ "$(pwd)" != $YAML_MIN_DIR ]]; then
-		__DD_POCKET="$(pwd)";
-		cd $YAML_DIR;
-	elif [[ -n $__DD_POCKET ]]; then
-		cd $__DD_POCKET;
-		__DD_POCKET="";
-	else;
-		echo "Nowhere to navigate.";
-	fi;
+	___dd $YAML_MIN_DIR;
 }
 
 dack() {
-	local target="$(basename $(pwd))_$1_1"
-	d logs -f $target --tail 1000 | ack --passthru "$2"
+	d logs -f --tail 1000 $1 | ack --passthru "$2"
 }
 dgrep() {
-	local target="$(basename $(pwd))_$1_1"
-	d logs -f $target --tail 1000 | grep -n "$2"
+	d logs -f --tail 1000 $1 | grep -n "$2"
 }
 
 dexec() {
-	local target="$(basename $(pwd))_$1_1"
-	bash -c "clear && docker exec -it $target sh"
+	bash -c "clear && docker exec -it $1 sh"
 }
 
-alias dtail_client="dack mercury-client '｢wdm｣'"
-alias dtail_server="dack mercury-server '###'"
+alias dack_cl="dack mercury-client '｢wdm｣'"
+alias dack_se="dack mercury-server '###'"
 
 alias dedangle="docker volume rm \"$(docker volume ls -qf dangling=true)\""
 alias sc="cd ~/secretcrimes"
